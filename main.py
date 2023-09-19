6f121d93a0df5e20dbe473b9c51b8b58754d4c29
Tung Nguyen <nvntung@gmail.com>
2023-09-19 22:59:46 +0100

    Set up SSH connection

--- main.py
+++ main.py
@@ -3,11 +3,31 @@ from fastapi import FastAPI
 import requests
 import os
 
+from subprocess import Popen, PIPE
+
 app = FastAPI()
 
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
+
+
+@app.get("/model/list")
+def list_biomodels():
+    # https://stackoverflow.com/a/10501355/865603
+    import uuid
+    filename = str(uuid.uuid4())
+
+    # https://www.bswen.com/2018/04/python-How-to-generate-random-large-file-using-python.html
+    import random
+    import string
+
+
+@app.get("/testssh")
+def test_ssh():
+    stdout, stderr = Popen(['ssh', 'codondc', 'cd /homes/tc_pst03/testbsub; sh task1.sh'], stdout=PIPE).communicate()
+    return { "output": stdout }
+
 
 @app.get("/model/{submission_id}")
 def download_model(submission_id: str):
